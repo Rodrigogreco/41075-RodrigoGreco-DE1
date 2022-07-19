38de9f5afd04fef3949bc536f9c912f6afd67f12
Rodrigo Greco <rodrigreco10@gmail.com>
2022-07-19 18:39:35 -0300

    parte final de la entrega

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function calcularMontoDeCF(precio){
+    return precio * 0.21;
+}
+
+let sumatoria = 0;
+let precioProductos
+do{
+    precioProductos = prompt('Si sos Responsable Inscripto ingresa el monto total de tu factura y te diremos el IVA CF que podes deducir en tu ddjj mensual de IVA');
+    const precioProductosParseado = parseFloat(precioProductos);
+    if(precioProductosParseado){
+        const productoIva = calcularMontoDeCF(precioProductosParseado)
+        sumatoria += productoIva
+        alert('Iva a Computar '+ productoIva)
+    }
+    
+}while(precioProductos != 'Listo')
+alert('El Importe a computar de CF es ' + sumatoria)
